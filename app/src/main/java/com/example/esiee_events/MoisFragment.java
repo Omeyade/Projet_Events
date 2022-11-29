92015794f9560b3730f2a8fdafa4cd503dd05e9d
Omeyade <jared.grace.2700@gmail.com>
2022-11-29 17:17:48 +0100

    Calender Month manually V3 classe jour

--- app/src/main/java/com/example/esiee_events/MoisFragment.java
+++ app/src/main/java/com/example/esiee_events/MoisFragment.java
@@ -1,5 +1,6 @@
 package com.example.esiee_events;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,34 +54,65 @@ public class MoisFragment extends Fragment {
         return fragment;
     }
 
-    @Override
+
+    /*@Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-        this.gridView = (GridView)findViewById(R.id.gridView);
-
-        ArrayList<Integer> Mois = new ArrayList<>(31);
-        for(int k=0; k<31;k++ ){
-            Mois.add(k+1);
-        }
-
-        ArrayAdapter<Integer> arrayAdapter
-                = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1, Mois);
-        gridView.setAdapter(arrayAdapter);
-
-
-
-    }
+    }*/
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View myView = inflater.inflate(R.layout.fragment_mois, container, false);
+        this.gridView = (GridView)myView.findViewById(R.id.gridView);
+        //Jour jourNov = new Jour(false, 0, 11, 2022, 0, 0, "", "", 0);
+
+        //creation d'un mois pour le test
+        ArrayList<Jour> Novembre = new ArrayList<>(30);
+        for(int k=0; k<30;k++ ){
+            Jour jourNov = new Jour(false, k+1, 11, 2022, 0, 0, "", "", "", 0);
+            if (k==5){
+                jourNov.setEvent(true);
+                jourNov.setHeure(18);
+                jourNov.setMinute(30);
+                jourNov.setNom("Karaoke");
+                jourNov.setLieu("Amphi 210");
+                jourNov.setCommentaire("Karaoke organisé par le Club Chorale");
+                jourNov.setPrix(0);
+            }
+            Novembre.add(jourNov);
+        }
+
+        //tableau d'entiers pour afficher dans le calendrier
+        ArrayList<Integer> Mois = new ArrayList<>(30);
+        for(int k=0; k<30;k++ ){
+            Mois.add(Novembre.get(k).jour);
+        }
+
+        //Changer la couleur de l'element si il y a un evenement ce jour là
+        for(int k=0; k<30; k++){
+            if(Novembre.get(k).isEvent()==true){
+                //gridView.getChildAt(k).setBackgroundColor(Color.parseColor("#18A608"));
+                gridView.getChildAt(k).setBackgroundColor(0xFF0000FF);
+                //Toast.makeText(getActivity(), Novembre.get(k).getNom(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        //Afficher le calendrier
+        ArrayAdapter<Integer> arrayAdapter
+                = new ArrayAdapter<Integer>(getActivity(), android.R.layout.simple_list_item_1, Mois);
+        gridView.setAdapter(arrayAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mois, container, false);
+        //return inflater.inflate(R.layout.fragment_mois, container, false);
+
+        return myView;
+
     }
 }
